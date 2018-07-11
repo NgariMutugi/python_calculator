8166acc855f04649326f5feca8e8ea47032d51cd
NgariMutugi <NgariMutugi@github.com>
2018-07-11 20:48:54 +0300

    building functions and deployment

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_calculator',
+    version='0.001',
+    packages=[''],
+    url='https://github.com/NgariMutugi/python_calculator.git',
+    license='free and opensource',
+    author='Eustus',
+    author_email='eustus@consultant.com',
+    description='calculator made in python code'
+)
